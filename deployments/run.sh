3732d654ebb62aa9679ccae7e43da1ba4720659c
Evy Bongers <evy@evybongers.nl>
2019-07-02 19:10:38 +0200

    Update deployment script to force linux toolchain

--- deployments/run.sh
+++ deployments/run.sh
@@ -3,6 +3,6 @@ set -eux
 
 cd "$(dirname "$(realpath "${0}")")"
 
-bazel run //cmd/go:go_container
+bazel run --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64 //cmd/go:go_container
 
 docker-compose up
